d9fcb8d5ec952ccde1dcde7e3e06d50d1419b314
Riyad Hosen <riyadhosen@Riyads-Air.netis.cc>
2024-03-16 06:48:59 -0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
       <NavBar></NavBar>
       {/* banner */}
       <Banner></Banner>
+      
       </body>
   
     </>
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
+
 const NavBar = () => {
   return (
     <div className="navbar bg-base-100 ">
       <div className="flex-1">
-        <a className="text-3xl font-bold">Recipe Calories</a>
+        <a className=" text-xl md:text-3xl font-bold">Recipe Calories</a>
       </div>
 
-      <div>
+      <div className=" hidden md: display">
         <ul className=" flex gap-4 justify-between items-center mr-11">
             <li>Home</li>
             <li>Recipes</li>
@@ -30,30 +31,12 @@ const NavBar = () => {
             role="button"
             className="btn btn-ghost btn-circle avatar"
           >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              />
-            </div>
+            
+                
+            
           </div>
-          <ul
-            tabIndex={0}
-            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
+         
+         
         </div>
       </div>
     </div>
